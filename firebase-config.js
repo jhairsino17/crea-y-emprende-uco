5327ebede98654c77860ecbf603b7486bc9db8d7
jhairsino17 <taywa1507@gmail.com>
2025-08-13 12:11:15 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.__firebase_config = JSON.stringify({
+  apiKey: "AIzaSyCI9UHmWfR8WGkbo-dNVGKwtynzEa_ButE",
+  authDomain: "emprendimieto-uco.firebaseapp.com",
+  projectId: "emprendimieto-uco",
+  storageBucket: "emprendimieto-uco.firebasestorage.app",
+  messagingSenderId: "567820490048",
+  appId: "1:567820490048:web:9d4415fde6de8b1df156a5",
+});
